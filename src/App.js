04881dc2f9041ab0b11801f1aae24ed1391f6a4f
nabeff <neffina8@gmail.com>
2023-10-31 21:45:46 +0100

    add note components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NoteList";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "15/04/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "16/04/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "17/04/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is my new note!",
+      date: "30/04/2023",
+    },
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} handleAddNote={addNote} />
     </div>
   );
 };
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { MdDeleteForever } from "react-icons/md";
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
     <div className="note">
-      <span>hello! this is our first note:</span>
+      <span>{text}</span>
       <div className="note-footer">
-        <small>10/31/2023</small>
+        <small>{date}</small>
         <MdDeleteForever className="delete-icon" size="1.3em" />
       </div>
     </div>
