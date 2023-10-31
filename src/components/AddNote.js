56c6082d52350dc4c689e7ae32920c7f19c9067d
nabeff <neffina8@gmail.com>
2023-10-31 21:52:43 +0100

    adding note character count

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 const AddNote = ({ handleAddNote }) => {
   const [noteText, setNoteText] = useState("");
+  const characterLimit = 200;
 
   const handleChange = (event) => {
-    setNoteText(event.target.value);
+    if (characterLimit - event.target.value.length >= 0) {
+      setNoteText(event.target.value);
+    }
   };
 
   const handleSaveClick = () => {
@@ -24,7 +27,7 @@ const AddNote = ({ handleAddNote }) => {
         onChange={handleChange}
       ></textarea>
       <div className="note-footer">
-        <small>200 Remaining</small>
+        <small>{characterLimit - noteText.length}</small>
         <button className="save" onClick={handleSaveClick}>
           Save
         </button>
